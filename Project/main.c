7d0a7552106546dcc22b83c66b66c8eb3a598a51
tomseipp <thisistomseipp@gmail.com>
2026-04-23 23:35:50 +0100

    stack smash yo mom

--- Project/main.c
+++ Project/main.c
@@ -158,3 +158,11 @@ int main(void){
    
 } // end of main
 
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
